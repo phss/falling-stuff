0d2b8c3e5ac7599512454e7aa9e191291549d438
Paulo Schneider <paulo.schneider@gmail.com>
2019-01-06 20:25:56 +0000

    Hacking about with the player controller

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
+    private float timeSinceLastDrop = 0;
+
     void Update() {
+        if ((Time.fixedTime - timeSinceLastDrop) > 1) {
+            transform.Translate(new Vector3(0, -1, 0));
+            timeSinceLastDrop = Time.fixedTime;
+        }
     }
 }
