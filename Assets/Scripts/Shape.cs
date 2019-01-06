b86ba2a89e9fd453cd10f86b3771a9dadda51123
Paulo Schneider <paulo.schneider@gmail.com>
2019-01-06 22:48:18 +0000

    Rotation S shape clockwise

---
+++ Assets/Scripts/Shape.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shape : MonoBehaviour {
+
+    private float timeSinceLastDrop = 0;
+
+    void Start() {
+        Debug.Log(transform.position);
+        Debug.Log(transform.GetChild(0).transform.position);
+        Debug.Log(transform.GetChild(1).transform.position);
+        Debug.Log(transform.GetChild(2).transform.position);
+        Debug.Log(transform.GetChild(3).transform.position);
+    }
+
+    void Update() {
+        if ((Time.fixedTime - timeSinceLastDrop) > 1) {
+            // transform.Translate(new Vector3(0, -1, 0));
+            // timeSinceLastDrop = Time.fixedTime;
+            RotateClockwise();
+            timeSinceLastDrop = Time.fixedTime;
+        }
+    }
+
+    void RotateClockwise() {
+        foreach (Transform child in transform) {
+            Vector3 normalizedPosition = child.position - transform.position;
+            Vector3 rotationInX = GetRotationInX(normalizedPosition.x);
+            Vector3 rotationInY = GetRotationInY(normalizedPosition.y);
+
+            child.position += rotationInX + rotationInY;
+        }
+    }
+
+    private Vector3 GetRotationInX(float x) {
+        return new Vector3(x * -1, x * -1, 0f);
+    }
+    private Vector3 GetRotationInY(float y) {
+        return new Vector3(y, y * -1, 0f);
+    }
+}
