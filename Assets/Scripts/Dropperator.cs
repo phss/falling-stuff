fd013437f373e48575722ae00e1d9adc9a426375
Paulo Schneider <paulo.schneider@gmail.com>
2019-01-08 18:53:32 +0000

    Totally drop on space

--- Assets/Scripts/Dropperator.cs
+++ Assets/Scripts/Dropperator.cs
@@ -8,13 +8,20 @@ public class Dropperator : Shape {
     private float timeSinceLastDrop = 0;
 
     void Update() {
-        if ((Time.fixedTime - timeSinceLastDrop) > 1f) {
-            bool didMove = AttemptMove(Vector3.down);
-            if (!didMove) {
-                board.Add(this);
-                Destroy(gameObject);
-            }
+        if (Input.GetButtonDown("Jump")) {
+            while(AttemptDrop());
+        } else if ((Time.fixedTime - timeSinceLastDrop) > 1f) {
+            AttemptDrop();
             timeSinceLastDrop = Time.fixedTime;
         }
+    }
+
+    private bool AttemptDrop() {
+        bool didMove = AttemptMove(Vector3.down);
+        if (!didMove) {
+            board.Add(this);
+            Destroy(gameObject);
+        }
+        return didMove;
     }
 }
