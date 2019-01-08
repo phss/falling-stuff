e20db593af5ff3aafdb0c26fbc0373d9eb37ea18
Paulo Schneider <paulo.schneider@gmail.com>
2019-01-08 22:17:08 +0000

    Basic block breaking

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -18,7 +18,24 @@ public class Board : MonoBehaviour {
         StartNewShape();
     }
 
+    public bool CanFitShape(Shape shape) {
+        foreach (Vector3 position in shape.GetBlockPositions()) {
+            bool outOfBounds = position.x < 0 || position.x >= dimensions.x || position.y < 0 || position.y >= dimensions.y;
+
+            if (outOfBounds || boardBlocks[(int) position.x, (int) position.y] != null) {
+                return false;
+            }
+        }
+        return true;
+    }
+
     public void Add(Shape shape) {
+        AddBlocksFrom(shape);
+        ClearCompletedRows();
+        StartNewShape();
+    }
+
+    private void AddBlocksFrom(Shape shape) {
         List<Transform> local = new List<Transform>();
         foreach (Transform block in shape.transform) {
             Vector3 blockPosition = shape.transform.localPosition + block.localPosition;
@@ -28,21 +45,41 @@ public class Board : MonoBehaviour {
         foreach (Transform block in local) {
             block.parent = transform;
         }
-        // ClearCompletedRows();
-        StartNewShape();
     }
 
-    public bool CanFitShape(Shape shape) {
-        foreach (Vector3 position in shape.GetBlockPositions()) {
-            if (position.x < 0 || position.x >= dimensions.x || position.y < 0 || position.y >= dimensions.y || boardBlocks[(int) position.x, (int) position.y] != null) {
+    private void ClearCompletedRows() {
+        for (int row = dimensions.y - 1; row >= 0; row--) {
+            if (IsRowFilled(row)) {
+                ClearRow(row);
+            }
+        }
+    }
+
+    private bool IsRowFilled(int row) {
+        for (int col = 0; col < dimensions.x; col++) {
+            if (boardBlocks[col, row] == null) {
                 return false;
             }
         }
         return true;
     }
 
-    private void ClearCompletedRows() {
+    private void ClearRow(int rowToDelete) {
+        for (int col = 0; col < dimensions.x; col++) {
+            Destroy(boardBlocks[col, rowToDelete].gameObject);
+        }
 
+        for (int row = rowToDelete; row < dimensions.y; row++) {
+            for (int col = 0; col < dimensions.x; col++) {
+                boardBlocks[col, row] = null;
+                if ((row + 1) < dimensions.y) {
+                    boardBlocks[col, row] = boardBlocks[col, row+1];
+                    if (boardBlocks[col, row] != null) {
+                        boardBlocks[col, row].position += Vector3.down;
+                    }
+                }
+            }
+        }
     }
 
     private void StartNewShape() {
