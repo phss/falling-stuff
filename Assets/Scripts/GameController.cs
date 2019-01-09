a6161b06fa3c2496d9614bbe875a4680fcd7484e
Paulo Schneider <paulo.schneider@gmail.com>
2019-01-09 21:34:01 +0000

    Basic pause screen

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -4,32 +4,54 @@ using UnityEngine;
 
 enum GameState {
     Splash,
-    Playing
+    Playing,
+    Paused
 }
 
 public class GameController : MonoBehaviour {
     public GameObject splashImage;
+    public GameObject pause;
     public GameObject board;
 
     private GameState state;
     private AudioSource startGameSound;
+    private AudioSource pauseSound;
 
     void Start() {
         state = GameState.Splash;
-        board.SetActive(false);
-        startGameSound = GetComponent<AudioSource>();
+        startGameSound = GetComponents<AudioSource>()[0];
+        pauseSound = GetComponents<AudioSource>()[1];
     }
 
     void Update() {
         if (state == GameState.Splash && Input.GetButtonDown("Submit")) {
-            state = GameState.Playing;
             StartGame();
+        } else if (state == GameState.Playing && Input.GetButtonDown("Cancel")) {
+            Pause();
+        } else if (state == GameState.Paused) {
+            if (Input.GetButtonDown("Cancel")) {
+                StartGame();
+            } else if (Input.GetButtonDown("Quit")) {
+                Application.Quit();
+            } else if (Input.GetButtonDown("Restart")) {
+                StartGame();
+            }
         }
     }
 
     private void StartGame() {
-        splashImage.SetActive(false);
-        board.SetActive(true);
+        state = GameState.Playing;
         startGameSound.Play();
+        splashImage.SetActive(false);
+        pause.SetActive(false);
+        board.SetActive(true);
     }
+
+    private void Pause() {
+        state = GameState.Paused;
+        pauseSound.Play();
+        board.SetActive(false);
+        pause.SetActive(true);
+    }
+
 }
