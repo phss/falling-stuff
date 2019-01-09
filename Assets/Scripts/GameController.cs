2be172e306a95b937f891d45deec35fd58a64586
Paulo Schneider <paulo.schneider@gmail.com>
2019-01-09 21:07:31 +0000

    Implement Splash screen

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+enum GameState {
+    Splash,
+    Playing
+}
+
+public class GameController : MonoBehaviour {
+    public GameObject splashImage;
+    public GameObject board;
+
+    private GameState state;
+    private AudioSource startGameSound;
+
+    void Start() {
+        state = GameState.Splash;
+        board.SetActive(false);
+        startGameSound = GetComponent<AudioSource>();
+    }
+
+    void Update() {
+        if (state == GameState.Splash && Input.GetButtonDown("Submit")) {
+            state = GameState.Playing;
+            StartGame();
+        }
+    }
+
+    private void StartGame() {
+        splashImage.SetActive(false);
+        board.SetActive(true);
+        startGameSound.Play();
+    }
+}
