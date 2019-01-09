a88ce7e41d7e68c563f9ebf3a61f72500f96b402
Paulo Schneider <paulo.schneider@gmail.com>
2019-01-09 15:30:26 +0000

    Smoother movement

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -4,12 +4,26 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Mover : Shape {
+    public float repeatMoveTime = 0.3f;
+    public float subsequentMovesTime = 0.05f;
+
     private float timeSinceLastMove = 0;
+    private int moves = 0;
 
     void Update() {
-        if ((Time.fixedTime - timeSinceLastMove) > 0.1f) {
-            int horizontal = (int) Input.GetAxis("Horizontal");
-            AttemptMove(new Vector3(horizontal, 0f, 0f));
+        int movement = (int) Input.GetAxis("Horizontal");
+        if (movement == 0) {
+            moves = 0;
+            return;
+        }
+
+        bool isStopped = moves == 0;
+        bool canDoFirstRepeatMove = moves == 1 && (Time.fixedTime - timeSinceLastMove) > repeatMoveTime;
+        bool canDoSubsequentMoves = moves > 1 && (Time.fixedTime - timeSinceLastMove) > subsequentMovesTime;
+
+        if (isStopped || canDoFirstRepeatMove || canDoSubsequentMoves) {
+            moves++;
+            AttemptMove(new Vector3(movement, 0f, 0f));
             timeSinceLastMove = Time.fixedTime;
         }
     }
