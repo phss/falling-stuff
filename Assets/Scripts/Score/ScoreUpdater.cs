1580df05300cb621d88000c60e550a87ee1bae05
Paulo Schneider <paulo.schneider@gmail.com>
2019-01-10 18:25:58 +0000

    Implement scalling score, based on level and number of lines cleared

--- Assets/Scripts/Score/ScoreUpdater.cs
+++ Assets/Scripts/Score/ScoreUpdater.cs
@@ -6,7 +6,9 @@ using UnityEngine.UI;
 public class ScoreUpdater : MonoBehaviour {
 
     private Text scoreText;
+    private int level = 1;
     private int score = 0;
+    private int[] scoreTable = new[] { 0, 40, 100, 300, 1200 }; 
 
     void Awake() {
        scoreText = GetComponent<Text>(); 
@@ -16,12 +18,16 @@ public class ScoreUpdater : MonoBehaviour {
     }
 
     private void ResetScore() {
+        level = 1;
         score = 0;
         UpdateScoreText();
     }
 
     private void AddToScore(int linesCleared) {
-        score += linesCleared;
+        if (linesCleared >= scoreTable.Length) {
+            linesCleared = scoreTable.Length - 1;
+        }
+        score += scoreTable[linesCleared] * level;
         UpdateScoreText();
     }
 
