73233a213677ec64f19b60a949e9f6eb3cc8232d
Paulo Schneider <paulo.schneider@gmail.com>
2019-01-10 18:20:00 +0000

    Add basic score counting lines cleared

---
+++ Assets/Scripts/Score/ScoreUpdater.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreUpdater : MonoBehaviour {
+
+    private Text scoreText;
+    private int score = 0;
+
+    void Awake() {
+       scoreText = GetComponent<Text>(); 
+
+       GameEvents.OnNewGame += ResetScore;
+       BoardEvents.OnLinesCleared += AddToScore;
+    }
+
+    private void ResetScore() {
+        score = 0;
+        UpdateScoreText();
+    }
+
+    private void AddToScore(int linesCleared) {
+        score += linesCleared;
+        UpdateScoreText();
+    }
+
+    private void UpdateScoreText() {
+        scoreText.text = score.ToString();
+    }
+}
